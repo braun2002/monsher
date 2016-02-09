1bfffcd3ee9ed128de0ee19a7993578e780add2c
Braun2002 <srg,tsygankov@gmail.com>
2016-02-09 19:45:42 +0300

    slider and simple carts

--- index.php
+++ index.php
@@ -1,2 +1,101 @@
-<?php
-echo "It's work";
+<html>
+<head>
+    <script src="script/jquery-1.7.2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+<header>
+    <div class="top">
+        <header class="cf">
+            <div class="logo"><img src="img/logo.png"/> </div>
+            <div class="navigation">
+                <nav>
+                    <a href="javascript:void(0)" class="smobitrigger ion-navicon-round"><span>Menu</span></a>
+                    <ul class="mobimenu">
+                        <li><a href="">Продукция</a></li>
+                        <li><a href="">О компании</a></li>
+                        <li><a href="">Контакты</a></li>
+                        <li><a href="">Где купить</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </div>
+    <div id="slider-wrap">
+        <div id="slider">
+            <div class="slide" id="s1">
+                <p>Какой то текст</p>
+
+            </div>
+            <div class="slide" id="s2">
+                <p>Some text 2 </p>
+            </div>
+            <div class="slide" id="s3">
+                <p>Some text 3</p>
+            </div>
+            <div class="arrow bounce">
+
+            </div>
+        </div>
+    </div>
+</header>
+<main class="main">
+
+    <div class="text_head">
+        <h1>Продукция</h1>
+    </div>
+
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+    <div class="cart">
+        <div class="img"><img src="img/kond1.jpg"></div>
+        <div class="name">Some Condition</div>
+        <button>Подробнее</button>
+    </div>
+</main>
+<footer>
+
+</footer>
+<script src="script/slider.js"></script>
+<script src="script/simpleMobileMenu.js"></script>
+<script>
+    $('.smobitrigger').smplmnu();
+</script>
+</body>
+</html>
